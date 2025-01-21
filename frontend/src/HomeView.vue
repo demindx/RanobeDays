8f4c751f14a53ee856ba4a2fb57f173f6461fa41
SerMeliodas <oliver9574@gmail.com>
2025-01-21 17:29:03 +0100

    feat(frontend): team ui element

--- frontend/src/HomeView.vue
+++ frontend/src/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Button from "@/components/ui/Button.vue"
 import Metadata from "@/components/ui/Metadata.vue"
+import Team from "@/components/ui/Team.vue"
 </script>
 
 <template class="">
   <Metadata>test</Metadata>
   <Button>Войти</Button>
+  <Team image="https://remanga.org/media/publishers/disguise/high_cover.jpg">team name</Team>
 </template>
---
+++ frontend/src/components/ui/Team.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  image: String
+})
+</script>
+
+<template>
+  <div class="w-fit">
+    <img :src="image" alt="" class="rounded-full size-24 shadow-inner bg-green-200 shadow-background">
+    <div class="w-full text-center">
+      <slot></slot>
+    </div>
+  </div>
+</template>
